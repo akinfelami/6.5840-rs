cb6760fc3fac19aef3bc979effc4c689a8abdf14
Akinfolami Akin-Alamu <aoa9@cornell.edu>
2026-04-12 14:38:06 -0700

    complete utils?

--- libs/mr/src/util.rs
+++ libs/mr/src/util.rs
@@ -1,4 +1,4 @@
-use std::{fs, path::Path, process::Stdio};
+use std::{fs, path::Path};
 
 use base64::{Engine, engine::general_purpose::URL_SAFE};
 use rand::RngExt;
@@ -71,11 +71,62 @@ pub fn coordinator_sock() -> String {
     s
 }
 
-// fn mk_correct_output(files: &[String], app: &str, out: &str) {
-//     let cmd = Command::new("cargo")
-//         .args(["run", "--bin", "mrsequential", app, sock])
+pub async fn mk_correct_output(files: &[String], app: &str, out: &str) {
+    let mut cmd = Command::new("cargo")
+        .args(["run", "--bin", "mrsequential", app])
+        .args(files)
+        .stdout(std::process::Stdio::inherit())
+        .stderr(std::process::Stdio::inherit())
+        .current_dir("tmp")
+        .spawn()
+        .expect("failed to start mrsequential");
 
-// }
+    let stat = cmd.wait().await.expect("mrsequential failed to run");
+    if !stat.success() {
+        panic!("mrsequential failed with status: {}", stat);
+    }
+
+    let out_path = Path::new("tmp").join(out);
+    let output_file =
+        std::fs::File::create(&out_path).unwrap_or_else(|_| panic!("create {:?} failed", out));
+
+    let out_path = Path::new("tmp").join("mr-out-0");
+    let mut cmd = Command::new("sort")
+        .arg(&out_path)
+        .stdout(output_file)
+        .stderr(std::process::Stdio::inherit())
+        .spawn()
+        .expect("sort failed to start");
+
+    let stat = cmd.wait().await.expect("sort failed to run");
+    if !stat.success() {
+        panic!("sort failed with status: {}", stat);
+    }
+
+    std::fs::remove_file(out_path).expect("unable to remove out file");
+}
+
+pub async fn merge_output(out: &str) {
+    let files = find_files("tmp", "mr-out-[0-9]").await;
+    if files.len() < 1 {
+        panic!("reduce created no mr-out-X output files!");
+    }
+    let output_path = Path::new("tmp").join(out);
+    let output_file =
+        std::fs::File::create(&output_path).unwrap_or_else(|_| panic!("create {:?} failed", out));
+
+    let mut cmd = Command::new("sort")
+        .args(&files)
+        .stdout(output_file)
+        .stderr(std::process::Stdio::inherit())
+        .spawn()
+        .expect("sort failed to start");
+
+    let stat = cmd.wait().await.expect("sort failed to run");
+    if !stat.success() {
+        panic!("sort failed with status: {}", stat);
+    }
+}
 
 pub async fn find_files(dir: &str, s: &str) -> Vec<String> {
     let cmd = Command::new("find")
@@ -113,12 +164,17 @@ pub async fn cleanup() {
     }
 }
 
-// TODO: check this for stdout and stderr reading
-pub fn run_cmp(f1: &str, f2: &str, msg: &str) {
-    let cmp_cmd = Command::new("cmp")
+pub async fn run_cmp(f1: &str, f2: &str, msg: &str) {
+    let mut cmp_cmd = Command::new("cmp")
         .args([f1, f2])
+        .current_dir("tmp")
         .spawn()
         .expect("failed to start cmp");
+
+    let stat = cmp_cmd.wait().await.expect("cmp failed to run");
+    if !stat.success() {
+        panic!("cmp failed with status: {}. {}", stat, msg);
+    }
 }
 
 pub fn count_pattern_file(f: &str, p: &str) -> usize {
@@ -132,27 +188,4 @@ pub fn count_pattern(files: &[String], p: &str) -> usize {
         n += count_pattern_file(f, p);
     }
     n
-}
-
-pub async fn merge_output(tmp: &str, out: &str) {
-    let files = find_files(tmp, "mr-out-[0-9]*").await;
-    if files.is_empty() {
-        panic!("reduce created no mr-out-X output files!");
-    }
-
-    let output_path = Path::new(tmp).join(out);
-    let output_file =
-        std::fs::File::create(&output_path).unwrap_or_else(|_| panic!("create {:?} failed", out));
-
-    let mut cmd = Command::new("sort")
-        .args(&files)
-        .stdout(output_file)
-        .stderr(std::process::Stdio::inherit())
-        .spawn()
-        .expect("sort failed to start");
-
-    let status = cmd.wait().await.expect("failed to wait on sort");
-    if !status.success() {
-        panic!("sort failed with status {:?}", status.code());
-    }
 }
